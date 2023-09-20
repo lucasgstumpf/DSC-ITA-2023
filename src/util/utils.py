bd34708e17c214dfc437d5c2fb55293c9ba426ef
lucasgstumpf <lucasgstumpf@gmail.com>
2023-09-19 22:37:05 -0300

    feat/moduleIngestao

--- src/util/utils.py
+++ src/util/utils.py
@@ -1,8 +1,18 @@
 import pandas as pd
 import os
+import pytz
+from datetime import datetime, timezone
 
 DATA_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'processed_data'))
 
+def toDate24(df,coluna):
+    brasilia_timezone = pytz.timezone('America/Sao_Paulo')
+
+    # Converte o timestamp para o fuso horário de Brasília
+    nova_coluna = coluna + "formatted"
+    df[nova_coluna] = df[coluna].apply(lambda x: datetime.fromtimestamp(int(x)/1000, tz=brasilia_timezone).strftime("%A, %B %d, %Y %H:%M:%S"))
+
+    return df
 
 def removeDuplicatesRows(df):
     # Remove as linhas duplicadas com base em todas as colunas
